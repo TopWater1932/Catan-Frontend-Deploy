1122a91e4a752e07928ca786d98424373d45fb7a
TopWater1932 <njcoelho97@gmail.com>
2025-11-28 15:49:35 +1100

    Built board and player initialisation API end point

--- app/Game.py
+++ app/Game.py
@@ -17,20 +17,47 @@ class Game:
     
     def setup(self):
         '''Set up the initial game state, including board configuration and player order.'''
-    
-        board = Board()
-        # Add tiles, nodes, paths as needed
+
+        # Board presets
         available_tiles = {
-            "Forest": 4,
-            "Hills": 3,
-            "Mountains": 3,
-            "Fields": 4,
-            "Pasture": 4,
-            "Desert": 1 
+            "wood": 4,
+            "brick": 3,
+            "ore": 3,
+            "wheat": 4,
+            "sheep": 4,
+            "desert": 1 
         }
         available_numbers = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12]
+        nodeRows = [7,9,11,11,9,7] # Index corresponds to row number, 0-indexed. Each integer is the number of nodes in each row.
+        rowNumberOffsets = [8,10,11,11,10,8] # Difference in node IDs between connected nodes in adjacent rows.
 
-        #create dictorionaries that store stup information about the tiles, nodes, and paths
+        # Setup board
+        tiles = self.setupTiles(available_tiles=available_tiles, available_numbers=available_numbers)
+        nodes, paths = self.setupNodesPaths(nodeRows=nodeRows, rowNumberOffsets=rowNumberOffsets).values()
+        # board = Board(tiles=tiles, nodes=nodes, paths=paths)
+
+        # Setup players
+        playerList = []
+        for i, userInput in enumerate(self.players):
+
+            player = Player(
+                f'p{i+1}',
+                userInput.get('name'),
+                userInput.get('colour'),
+                {"wood":0,"brick":0,"wheat":0,"sheep":0,"ore":0},
+                {"knight":0,"victoryPoint":0,"roadBuilding":0,"yearOfPlenty":0,"monopoly":0,"knightsPlayed":0},
+                {"roads":15,"settlements":5,"cities":4},
+                0
+            )
+            playerList.append(player)
+        
+        return {
+            'tiles':tiles,
+            'nodes':nodes,
+            'paths':paths,
+            'players':playerList,
+            'turn': playerList[self.current_turn].id
+        }
 
     def setupTiles(self, available_tiles, available_numbers):
         total_tiles = sum(available_tiles.values())
@@ -42,13 +69,13 @@ class Game:
             
             terrain = random.choice(list(available_tiles.keys()))
 
-            if(terrain == "Desert"):
+            if(terrain == "desert"):
                 num = None
             else:
                 num = random.choice(available_numbers)
                 available_numbers.remove(num)
 
-            tile = Tile(0, 0, terrain, None, num)
+            tile = Tile(f'Tile-{count}',0, 0, terrain, None, num)
             tileList.append(tile)
             available_tiles[terrain] -= 1
             if(available_tiles[terrain] == 0):
@@ -117,39 +144,23 @@ class Game:
 
         return {'nodes':nodeList, 'paths':pathList}
 
-
-    def nextTurn(self):
-        self.current_turn = (self.current_turn + 1) % len(self.players)
-
-
-game = Game(None, [Player("Alice", "red")])
-available_tiles = {
-            "Forest": 4,
-            "Hills": 3,
-            "Mountains": 3,
-            "Fields": 4,
-            "Pasture": 4,
-            "Desert": 1 
-        }
-available_numbers = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12]
-
-tiles = game.setupTiles(available_tiles=available_tiles, available_numbers=available_numbers)
-
-nodeRows = [7,9,11,11,9,7] # Index corresponds to row number, 0-indexed. Each integer is the number of nodes in each row.
-rowNumberOffsets = [8,10,11,11,10,8] # Difference in node IDs between connected nodes in adjacent rows.
-nodes, paths = game.setupNodesPaths(nodeRows=nodeRows, rowNumberOffsets=rowNumberOffsets).values()
+    def nextTurn(self,playerList):
+        self.current_turn = (self.current_turn + 1) % len(playerList)
 
 
-############# Print for checking initialised board setup #############
+# ############# Print for checking initialised board setup #############
 
-for node in nodes:
-    print("Node ID:", node.id, "Row:", node.row, "Neighbors:", node.neighbors)
-print("Total Nodes:", len(nodes))
+# for node in nodes:
+#     print("Node ID:", node.id, "Row:", node.row, "Neighbors:", node.neighbors)
+# print("Total Nodes:", len(nodes))
 
-for path in paths:
-    print("Path ID:", path.id, "connectedNodes:", path.connectedNodes)
-print("Total Paths:", len(paths))
+# for path in paths:
+#     print("Path ID:", path.id, "connectedNodes:", path.connectedNodes)
+# print("Total Paths:", len(paths))
 
 
 # for tile in tiles:
 #     print(tile.terrain_type, tile.number_token)
+
+game = Game(None, [{'name':'Alice', 'colour':'red'}, {'name':'Bob', 'colour':'blue'}, {'name':'Rob', 'colour':'green'}, {'name':'Sherry', 'colour':'white'}])
+initialisePackage = game.setup()
--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
+from Game import Game
+from Player import Player
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, World!"}
+@app.post("/init-inputs")
+async def handleInitInputs():
+    # Process initialization inputs here
+    return {"message": "Initialisation inputs received."}
+
+
+@app.get("/initialise")
+async def initialise():
+    initialised = False
+
+    # Create a new game instance and intialise it. Only do it once, otherwise provide the same info.
+    if initialised == False:
+        game = Game(None, [{'name':'Alice', 'colour':'red'}, {'name':'Bob', 'colour':'blue'}, {'name':'Rob', 'colour':'green'}, {'name':'Sherry', 'colour':'white'}])
+        initialisePackage = game.setup()
+
+        initialised = True
+
+    return initialisePackage
