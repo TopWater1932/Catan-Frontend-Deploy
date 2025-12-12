efcfc090fe3c203cedc40833b40bf63ee47bc8cc
Tong Chen <tche0609@uni.sydney.edu.au>
2025-12-12 18:48:52 +1100

    updated Node class from merge

--- backend/app/Node.py
+++ backend/app/Node.py
@@ -1,11 +1,20 @@
 class Node:
-    def __init__(self, id, occupiedBy=None, isBuildable=True, buildingType=None, leftPath=None, rightPath=None, verticalPath=None):
+    def __init__(self, id, occupiedBy=None, isBuildable=True, buildingType=None, paths=[]):
+        self.next = None
         self.id = id
+        self.paths = []
+        
+    def updateNeighbors(self):
+        for path in self.paths:
+            for node in path.connectedNodes:
+                if node != self:
+                    node.isBuildable = False
 
     def build(self, player, buildingType):
-        if self.isBuildable is True:
+        if self.occupiedBy is None and self.isBuildable:
             self.occupiedBy = player
             self.buildingType = buildingType
-            self.isBuildable = False
+            self.updateNeighbors()
             return True
         return False
+    
